4a01a873d1141da0c3214cfe2910e710058d5bc6
bo.song <bo.song@gotogameday.com>
2023-10-27 17:29:39 +0800

    add filies

--- vedio_study/docker_demo/dockerfile_os/docker-image-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t os_linux_python310:base .
-
-
--- vedio_study/flask_demo/get_post.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-# res = requests.post('http://127.0.0.1:8080/')
-res = requests.get('http://127.0.0.1:8080/')
-print(res.text)
